da0d8771971d34e47a8984bfef2ed9216172e47d
duol9 <lhyy0195@gmail.com>
2025-06-17 00:28:45 +0900

    :spakles: feat: Add getCourseList metho to CourseService

--- src/main/java/com/saisai/domain/course/service/CourseService.java
+++ src/main/java/com/saisai/domain/course/service/CourseService.java
@@ -1,10 +1,26 @@
 package com.saisai.domain.course.service;
 
+import com.saisai.domain.course.dto.response.GetCourseListRes;
+import com.saisai.domain.course.entity.CourseApi;
+import com.saisai.domain.course.repository.CourseRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONObject;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
+@Transactional(readOnly = true)
 public class CourseService {
 
+    private final CourseRepository courseRepository;
+    private final CourseApi courseApi;
+
+    public Page<GetCourseListRes> getCoursess(int page) throws Exception {
+        JSONObject courseBody = courseApi.callCourseAPI(page-1);
+        return GetCourseListRes.createCourseList(courseBody);
+    }
 }
