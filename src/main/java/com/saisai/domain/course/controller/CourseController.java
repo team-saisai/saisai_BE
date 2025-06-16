20dca38dbd5cd992d17af97c011d405f66d29ac5
duol9 <lhyy0195@gmail.com>
2025-06-17 05:02:38 +0900

    :sparkles: feat: Add getCourseInfo mthod to CourseController

--- src/main/java/com/saisai/domain/course/controller/CourseController.java
+++ src/main/java/com/saisai/domain/course/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.saisai.domain.course.controller;
 
+import static com.saisai.domain.common.response.SuccessCode.COURSE_INFO_GET_SUCCESS;
 import static com.saisai.domain.common.response.SuccessCode.COURSE_LIST_GET_SUCCESS;
 
 import com.saisai.domain.common.response.ApiResponse;
@@ -14,6 +15,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -37,5 +39,15 @@ public class CourseController {
     ) throws Exception {
         return ResponseEntity.status(HttpStatus.OK)
             .body(ApiResponse.success(COURSE_LIST_GET_SUCCESS, courseService.getCourses(page)));
+    }
+
+    @Operation(summary = "코스 상세 조회",
+        description = "코스ID, 코스명, 코스 설명, 난이도, 거리(km), 예상 소요시간(분), 시군(지역), 투어 정보, 여행자 정보, gpx경로, 완주자 수, 리워드 정보 반환 ")
+    @GetMapping("/{courseName}")
+    public ResponseEntity<ApiResponse<GetCourseInfoRes>> getCourseInfo(
+        @PathVariable String courseName
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success(COURSE_INFO_GET_SUCCESS, courseService.getCourseInfo(courseName)));
     }
 }
