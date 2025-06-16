c535a5d0d92c916269b393b8a79a89a07a3fc6ee
duol9 <lhyy0195@gmail.com>
2025-06-17 05:05:43 +0900

    :sparkles: feat: Add getCourseInfo mthod to CourseService

--- src/main/java/com/saisai/domain/course/service/CourseService.java
+++ src/main/java/com/saisai/domain/course/service/CourseService.java
@@ -1,10 +1,16 @@
 package com.saisai.domain.course.service;
 
+import com.saisai.domain.challenge.entity.Challenge;
+import com.saisai.domain.challenge.entity.ChallengeStatus;
+import com.saisai.domain.challenge.repository.ChallengeRepository;
+import com.saisai.domain.course.dto.response.GetCourseInfoRes;
 import com.saisai.domain.course.dto.response.GetCourseListRes;
 import com.saisai.domain.course.entity.CourseApi;
 import com.saisai.domain.course.repository.CourseRepository;
+import com.saisai.domain.ride.repository.RideRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Service;
@@ -17,10 +23,23 @@ import org.springframework.transaction.annotation.Transactional;
 public class CourseService {
 
     private final CourseRepository courseRepository;
+    private final RideRepository rideRepository;
+    private final ChallengeRepository challengeRepository;
     private final CourseApi courseApi;
 
-    public Page<GetCourseListRes> getCoursess(int page) throws Exception {
-        JSONObject courseBody = courseApi.callCourseAPI(page-1);
+    public Page<GetCourseListRes> getCourses(int page) throws Exception {
+        JSONObject courseBody = courseApi.callCourseApiBody(page-1);
         return GetCourseListRes.createCourseList(courseBody);
+    }
+
+    public GetCourseInfoRes getCourseInfo(String courseName) {
+        JSONArray courseItem = courseApi.callCourseApiItems(courseName);
+
+        Long completeUserCount = rideRepository.countByCourseNameAndStatus(courseName);
+
+        Challenge challenge = challengeRepository.findByCourseNameAndStatus(courseName,
+            ChallengeStatus.ONGOING);
+
+        return GetCourseInfoRes.createCourseInfo(courseItem, completeUserCount, challenge);
     }
 }
---
+++ src/main/java/com/saisai/domain/ride/repository/RideRepositoryCustom.java
@@ -0,0 +1,6 @@
+package com.saisai.domain.ride.repository;
+
+public interface RideRepositoryCustom {
+
+    Long countByCourseNameAndStatus(String courseName);
+}
