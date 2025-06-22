38681b22df66ebda35d3b4020421332d93cfd2d7
duol9 <lhyy0195@gmail.com>
2025-06-22 17:26:30 +0900

    :sparkles: feat: 코스 목록 조회 서비스 로직 추가

--- src/main/java/com/saisai/domain/challenge/repository/ChallengeRepository.java
+++ src/main/java/com/saisai/domain/challenge/repository/ChallengeRepository.java
@@ -1,8 +1,10 @@
 package com.saisai.domain.challenge.repository;
 
 import com.saisai.domain.challenge.entity.Challenge;
+import com.saisai.domain.challenge.entity.ChallengeStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ChallengeRepository extends JpaRepository<Challenge, Long> {
-
+public interface ChallengeRepository extends JpaRepository<Challenge, Long>, ChallengeRepositoryCustom {
+    Challenge findByCourseNameAndStatus (String courseName, ChallengeStatus status);
+    Challenge findByCourseName (String courseName);
 }
---
+++ src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryImpl.java
@@ -0,0 +1,4 @@
+package com.saisai.domain.challenge.repository;
+
+public class ChallengeRepositoryImpl {
+}
