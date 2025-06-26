50c1c74d522aabfc5732a9cc15192f086abfbb40
duol9 <lhyy0195@gmail.com>
2025-06-26 21:53:29 +0900

    :sparkles: feat: 잔행 중(ONGOING)인 챌린지 코스명 조회 쿼리 추가

--- src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryCustom.java
+++ src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryCustom.java
@@ -1,4 +1,8 @@
 package com.saisai.domain.challenge.repository;
 
+import com.saisai.domain.challenge.entity.ChallengeStatus;
+import java.util.List;
+
 public interface ChallengeRepositoryCustom {
+    List<String> findChallengeByStatus(ChallengeStatus status);
 }
--- src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryImpl.java
+++ src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryImpl.java
@@ -1,4 +1,24 @@
 package com.saisai.domain.challenge.repository;
 
-public class ChallengeRepositoryImpl {
+import static com.saisai.domain.challenge.entity.QChallenge.challenge;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.saisai.domain.challenge.entity.ChallengeStatus;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+@Repository
+@RequiredArgsConstructor
+public class ChallengeRepositoryImpl implements ChallengeRepositoryCustom {
+
+    private final JPAQueryFactory queryFactory;
+
+    @Override
+    public List<String> findChallengeByStatus(ChallengeStatus status) {
+        return queryFactory.select(challenge.courseName)
+            .from(challenge)
+            .where(challenge.status.eq(ChallengeStatus.ONGOING))
+            .fetch();
+    }
 }
