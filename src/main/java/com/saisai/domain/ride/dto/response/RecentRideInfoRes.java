98c276c372d42d6fd4863bad95d5d7d606674f9a
duol9 <lhyy0195@gmail.com>
2025-06-29 05:31:25 +0900

    :sparkles: feat: 최근 라이딩 한 코스 ResponseDto 추가

---
+++ src/main/java/com/saisai/domain/ride/dto/response/RecentRideInfoRes.java
@@ -0,0 +1,32 @@
+package com.saisai.domain.ride.dto.response;
+
+import com.saisai.domain.course.api.CourseItem;
+import com.saisai.domain.course.entity.CourseImage;
+import com.saisai.domain.ride.entity.Ride;
+import java.time.LocalDate;
+import java.util.Optional;
+
+public record RecentRideInfoRes(
+    String courseName,
+    String sigun,
+    String courseIamgeUrl,
+    Double distance,
+    Double progressRate,
+    LocalDate recentRideAt
+) {
+
+    public static RecentRideInfoRes from(Ride ride, CourseItem courseItem, CourseImage courseImage) {
+        String courseImageUrl = Optional.ofNullable(courseImage)
+            .map(CourseImage::getUrl)
+            .orElse(null);
+
+        return new RecentRideInfoRes(
+            courseItem.courseName(),
+            courseItem.sigun(),
+            courseImageUrl,
+            courseItem.distance(),
+            ride.getProgressRate(),
+            ride.getModifiedAt().toLocalDate()
+        );
+    }
+}
