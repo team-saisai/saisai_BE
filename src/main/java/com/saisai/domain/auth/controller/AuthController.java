c81f00874cf204f1583f31695aaf4402abfc4ca9
duol9 <lhyy0195@gmail.com>
2025-06-29 04:16:31 +0900

    :sparkles: feat: 회원가입 api 추가

--- src/main/java/com/saisai/domain/auth/controller/AuthController.java
+++ src/main/java/com/saisai/domain/auth/controller/AuthController.java
@@ -1,6 +1,22 @@
 package com.saisai.domain.auth.controller;
 
+import static com.saisai.domain.common.response.SuccessCode.LOGIN_SUCCESS;
+import static com.saisai.domain.common.response.SuccessCode.REGISTER_SUCCESS;
+import static com.saisai.domain.common.response.SuccessCode.REISSUE_SUCCESS;
+
+import com.saisai.domain.auth.dto.request.LoginReq;
+import com.saisai.domain.auth.dto.request.RegisterReq;
+import com.saisai.domain.auth.dto.response.TokenRes;
+import com.saisai.domain.auth.service.AuthService;
+import com.saisai.domain.common.response.ApiResponse;
+import io.swagger.v3.oas.annotations.Operation;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,5 +25,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class AuthController {
 
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    @Operation(summary = "회원가입",
+        description = "이메일, 닉네임, 비밀번호, 유저롤(USER/ADMIN 입력 필요")
+    public ResponseEntity<ApiResponse<TokenRes>> register(
+        @Valid @RequestBody RegisterReq registerReq
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success(REGISTER_SUCCESS, authService.register(registerReq)));
+    }
 
 }
