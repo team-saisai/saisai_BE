a9b16cc858aa4704e8b49e95d1818c1465c0e39d
duol9 <lhyy0195@gmail.com>
2025-06-29 05:31:03 +0900

    :sparkles: feat: 최근 라이딩 한 코스 조회 api 추가

---
+++ src/main/java/com/saisai/domain/ride/controller/MyRideController.java
@@ -0,0 +1,34 @@
+package com.saisai.domain.ride.controller;
+
+import static com.saisai.domain.common.response.SuccessCode.RECENT_RIDE_COURSE_GET_SUCCESS;
+
+import com.saisai.config.jwt.AuthUserDetails;
+import com.saisai.domain.common.annotation.Auth;
+import com.saisai.domain.common.response.ApiResponse;
+import com.saisai.domain.ride.dto.response.RecentRideInfoRes;
+import com.saisai.domain.ride.service.MyRideService;
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/my/rides")
+@RequiredArgsConstructor
+public class MyRideController {
+
+    private final MyRideService myRideService;
+
+    @Operation(summary = "최근 라이딩 한 코스 조회",
+        description = "코스명, 시군, 코스 이미지, 최근 주행일, 총 거리(km), 완주율 반환")
+    @GetMapping
+    public ResponseEntity<ApiResponse<RecentRideInfoRes>> getRecentRideInfo(
+        @Auth AuthUserDetails authUserDetails
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success(RECENT_RIDE_COURSE_GET_SUCCESS, myRideService.getRecentRideInfo(authUserDetails)));
+    }
+}
