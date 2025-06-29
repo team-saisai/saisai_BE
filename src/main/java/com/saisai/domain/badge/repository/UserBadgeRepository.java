ecab519149a3ce87ff6ba056c299e734944f04cc
duol9 <lhyy0195@gmail.com>
2025-06-30 03:29:45 +0900

    :sparkles: feat: 뱃지 상세 조회 메서드 추가

--- src/main/java/com/saisai/domain/badge/repository/UserBadgeRepository.java
+++ src/main/java/com/saisai/domain/badge/repository/UserBadgeRepository.java
@@ -1,8 +1,11 @@
 package com.saisai.domain.badge.repository;
 
 import com.saisai.domain.badge.entity.UserBadge;
+import com.saisai.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserBadgeRepository extends JpaRepository<UserBadge, Long>, UserBadgeRepositoryCustom{
 
+    Optional<UserBadge> findByUserAndId(User user, Long id);
 }
