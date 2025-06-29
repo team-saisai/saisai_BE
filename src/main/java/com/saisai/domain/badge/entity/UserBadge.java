0c7578631ad5081df5ba4c8bdf02ca5cd12ed7ac
duol9 <lhyy0195@gmail.com>
2025-06-29 23:02:30 +0900

    :sparkles: feat: UserBadge Entity 추가

---
+++ src/main/java/com/saisai/domain/badge/entity/UserBadge.java
@@ -0,0 +1,49 @@
+package com.saisai.domain.badge.entity;
+
+import com.saisai.domain.common.BaseEntity;
+import com.saisai.domain.user.entity.User;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "user_badges", uniqueConstraints =
+    @UniqueConstraint(
+        name = "USER_BADGE_UNIQUE",
+        columnNames = {"badge_id", "user_id"}
+    )
+)
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserBadge extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "badge_id", nullable = false)
+    private Badge badge;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Builder
+    public UserBadge(Badge badge, User user) {
+        this.badge = badge;
+        this.user = user;
+    }
+}
