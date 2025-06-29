f1ebf4fec5dbc2b0b1ffbe76b8e77fa640ff54af
duol9 <lhyy0195@gmail.com>
2025-06-30 03:26:24 +0900

    :recycle: refactor: 뱃지 리스트 조회 Response Dto 수정

--- src/main/java/com/saisai/domain/badge/dto/response/BadgeSummaryRes.java
+++ src/main/java/com/saisai/domain/badge/dto/response/BadgeSummaryRes.java
@@ -3,7 +3,7 @@ package com.saisai.domain.badge.dto.response;
 import com.querydsl.core.annotations.QueryProjection;
 
 public record BadgeSummaryRes(
-    Long badgeId,
+    Long userBadgeId,
     String badgeName,
     String badgeImageUrl
 ) {
