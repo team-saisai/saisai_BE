0331522e9a20adb0f9f78ee3b0312994ae986212
duol9 <lhyy0195@gmail.com>
2025-07-02 04:44:26 +0900

    :recycle: refactor: Course PK 변경에 의한 countByCourseIdAndStatus() 메서드 수정

--- src/main/java/com/saisai/domain/ride/repository/RideRepositoryImpl.java
+++ src/main/java/com/saisai/domain/ride/repository/RideRepositoryImpl.java
@@ -14,11 +14,11 @@ public class RideRepositoryImpl implements RideRepositoryCustom {
     private final JPAQueryFactory jpaQueryFactory;
 
     @Override
-    public Long countByCourseNameAndStatus(String courseName, RideStatus status) {
+    public Long countByCourseIdAndStatus(Long courseId, RideStatus status) {
         return jpaQueryFactory
             .select(ride.id.count())
             .from(ride)
-            .where(ride.courseName.eq(courseName)
+            .where(ride.course.id.eq(courseId)
                 .and(ride.status.eq(status)))
             .fetchOne();
     }
