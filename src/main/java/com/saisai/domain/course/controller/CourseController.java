52c30a996721d11e3675f3aff696b71d1358f4e0
duol9 <lhyy0195@gmail.com>
2025-07-02 04:26:49 +0900

    :recycle: refactor: Course 기본키 변경에 의한 API 파라미터 외 수정

--- src/main/java/com/saisai/domain/course/controller/CourseController.java
+++ src/main/java/com/saisai/domain/course/controller/CourseController.java
@@ -4,14 +4,16 @@ import static com.saisai.domain.common.response.SuccessCode.COURSE_INFO_GET_SUCC
 import static com.saisai.domain.common.response.SuccessCode.COURSE_LIST_GET_SUCCESS;
 
 import com.saisai.domain.common.response.ApiResponse;
-import com.saisai.domain.course.dto.response.CourseInfoRes;
-import com.saisai.domain.course.dto.response.CourseListItemRes;
+import com.saisai.domain.course.dto.response.CourseDetailsRes;
+import com.saisai.domain.course.dto.response.CoursePageRes;
 import com.saisai.domain.course.service.CourseService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -32,21 +34,23 @@ public class CourseController {
         description = "코스명, 요약, 난이도, 거리(km), 예상 소요시간(분), 시군, 도전자 수, 완주자 수, 챌린지 상태, 챌린지 종료일을 한 페이지 당 10개 씩 반환\n\n" +
             "페이지 번호 파라미터만 활성화 상태")
     @GetMapping
-    public ResponseEntity<ApiResponse<Page<CourseListItemRes>>> getAllCourses(
+    public ResponseEntity<ApiResponse<Page<CoursePageRes>>> getAllCourses(
         @Parameter(description = "페이지 번호") @RequestParam(defaultValue = "1") int page,
-        @Parameter(description = "챌린지 상태 (챌린지 중 = ONGOING). 미입력시 모든 코스 조회") @RequestParam(required = false) String status
+        @Parameter(description = "챌린지 상태 (챌린지 중 = ONGOING). 미입력시 모든 코스 조회") @RequestParam(required = false) String challengeStatus
     ) {
+
+        Pageable pageable = PageRequest.of(page - 1, 10);
         return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success(COURSE_LIST_GET_SUCCESS, courseService.getCourses(page, status)));
+            .body(ApiResponse.success(COURSE_LIST_GET_SUCCESS, courseService.getCourses(pageable, challengeStatus)));
     }
 
     @Operation(summary = "코스 상세 조회",
         description = "코스ID, 코스명, 코스 설명, 난이도, 거리(km), 예상 소요시간(분), 시군(지역), 투어 정보, 여행자 정보, gpx경로, 완주자 수, 리워드 정보 반환 ")
-    @GetMapping("/{courseName}")
-    public ResponseEntity<ApiResponse<CourseInfoRes>> getCourseInfo(
-        @PathVariable String courseName
+    @GetMapping("/{courseId}")
+    public ResponseEntity<ApiResponse<CourseDetailsRes>> getCourseInfo(
+        @PathVariable Long courseId
     ) {
         return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success(COURSE_INFO_GET_SUCCESS, courseService.getCourseInfo(courseName)));
+            .body(ApiResponse.success(COURSE_INFO_GET_SUCCESS, courseService.getCourseInfo(courseId)));
     }
 }
