6315309a18cf6f74af953ff7351c7cb2370f9937
duol9 <lhyy0195@gmail.com>
2025-07-02 04:24:13 +0900

    :sparkles: feat: Course 기본키 변경에 의한 Dto 필드 추가

--- src/main/java/com/saisai/domain/challenge/dto/response/PopularChallengeListItemRes.java
+++ src/main/java/com/saisai/domain/challenge/dto/response/PopularChallengeListItemRes.java
@@ -1,11 +1,13 @@
 package com.saisai.domain.challenge.dto.response;
 
+import com.saisai.domain.challenge.dto.projection.ChallengeInfoProjection;
 import com.saisai.domain.challenge.entity.ChallengeStatus;
-import com.saisai.domain.course.dto.response.CourseSummaryInfo;
+import com.saisai.domain.course.dto.response.CourseSummaryRes;
 import java.time.LocalDateTime;
 
 
 public record PopularChallengeListItemRes(
+    Long courseId,
     String courseName,
     Integer level,
     Double distance,
@@ -14,23 +16,24 @@ public record PopularChallengeListItemRes(
     String imageUrl,
     ChallengeStatus challengeStatus,
     LocalDateTime endedAt,
-    Long participantCount
+    Long courseChallengerCount
 ) {
 
     public static PopularChallengeListItemRes from(
         ChallengeInfoProjection challengeInfoProjection,
-        CourseSummaryInfo courseSummaryInfo
+        CourseSummaryRes courseSummaryRes
     ) {
         return new PopularChallengeListItemRes(
-            challengeInfoProjection.courseName(),
-            courseSummaryInfo.level(),
-            courseSummaryInfo.distance(),
-            courseSummaryInfo.estimatedTime(),
-            courseSummaryInfo.sigun(),
-            courseSummaryInfo.imageUrl(),
+            challengeInfoProjection.courseId(),
+            courseSummaryRes.courseName(),
+            courseSummaryRes.level(),
+            courseSummaryRes.distance(),
+            courseSummaryRes.estimatedTime(),
+            courseSummaryRes.sigun(),
+            courseSummaryRes.imageUrl(),
             challengeInfoProjection.challengeStatus(),
             challengeInfoProjection.endedAt(),
-            challengeInfoProjection.participantCount()
+            courseSummaryRes.courseChallengerCount()
         );
     }
 }
