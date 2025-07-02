635ffa85a95f3e634d60002735d7bc0625ebadc5
duol9 <lhyy0195@gmail.com>
2025-07-03 02:00:17 +0900

    :sparkles: feat: 인기 챌린지 조회 DSL 메서드 추가

--- src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryCustom.java
+++ src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryCustom.java
@@ -1,4 +1,11 @@
 package com.saisai.domain.challenge.repository;
 
+import com.saisai.domain.challenge.dto.projection.PopularChallengeProjection;
+import java.util.List;
+
 public interface ChallengeRepositoryCustom {
+
+    // 챌린지 진행 중인 코스 중에서 참가자가 많은 순으로 정렬 후 반환하는 메서드
+    List<PopularChallengeProjection> findTop10CoursesByOngoingChallengeRides();
+
 }
--- src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryImpl.java
+++ src/main/java/com/saisai/domain/challenge/repository/ChallengeRepositoryImpl.java
@@ -1,6 +1,14 @@
 package com.saisai.domain.challenge.repository;
 
+import static com.saisai.domain.challenge.entity.QChallenge.challenge;
+import static com.saisai.domain.ride.entity.QRide.ride;
+
 import com.querydsl.jpa.impl.JPAQueryFactory;
+import com.saisai.domain.challenge.dto.projection.PopularChallengeProjection;
+import com.saisai.domain.challenge.dto.projection.QPopularChallengeProjection;
+import com.saisai.domain.challenge.entity.ChallengeStatus;
+import com.saisai.domain.ride.entity.RideStatus;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +17,32 @@ import org.springframework.stereotype.Repository;
 public class ChallengeRepositoryImpl implements ChallengeRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
+
+    // 챌린지 진행 중인 코스 중에서 참가자가 많은 순으로 정렬 후 반환하는 메서드
+    @Override
+    public List<PopularChallengeProjection> findTop10CoursesByOngoingChallengeRides() {
+        return queryFactory
+            .select(new QPopularChallengeProjection(
+                challenge.id,
+                challenge.course.id,
+                challenge.status,
+                challenge.endedAt,
+                ride.id.count().coalesce(0L)
+            ))
+            .from(challenge)
+            .leftJoin(ride).on(
+                ride.course.id.eq(challenge.course.id)
+                    .and(ride.status.eq(RideStatus.IN_PROGRESS))
+            )
+            .where(challenge.status.eq(ChallengeStatus.ONGOING))
+            .groupBy(
+                challenge.id,
+                challenge.course.id,
+                challenge.status,
+                challenge.endedAt
+            )
+            .orderBy(ride.count().coalesce(0L).desc())
+            .limit(10)
+            .fetch();
+    }
 }
