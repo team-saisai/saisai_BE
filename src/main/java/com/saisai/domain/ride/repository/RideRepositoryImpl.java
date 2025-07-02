752f5696f3cc64cc6879d0604201f642d35e41ee
duol9 <lhyy0195@gmail.com>
2025-07-03 02:54:34 +0900

    :broom: chore: countByCourseIdAndStatus 메서드 주석 추가

--- src/main/java/com/saisai/domain/ride/repository/RideRepositoryImpl.java
+++ src/main/java/com/saisai/domain/ride/repository/RideRepositoryImpl.java
@@ -13,6 +13,7 @@ public class RideRepositoryImpl implements RideRepositoryCustom {
 
     private final JPAQueryFactory jpaQueryFactory;
 
+    // courseId 기준, Ride 상태 기반 사용자 수 반환 메서드
     @Override
     public Long countByCourseIdAndStatus(Long courseId, RideStatus status) {
         return jpaQueryFactory
