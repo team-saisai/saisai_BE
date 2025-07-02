8900aeb961343240a65f3d2d943ef2747c84191e
duol9 <lhyy0195@gmail.com>
2025-07-03 02:53:59 +0900

    :spakles: feat: countRideByCourseId JPQL 추가

--- src/main/java/com/saisai/domain/ride/repository/RideRepository.java
+++ src/main/java/com/saisai/domain/ride/repository/RideRepository.java
@@ -15,7 +15,7 @@ public interface RideRepository extends JpaRepository<Ride, Long>, RideRepositor
     // 최근 라이드한 코스 조회 메서드
     Ride findTop1ByUserIdOrderByModifiedAtDesc(Long id);
 
-    // courseId와 일치하는 도전자, 완주자 수 조회 메서드
+    // courseId와 일치하는 도전자, 완주자 수 조회 메서드 (리스트)
     @Query("""
         SELECT NEW com.saisai.domain.ride.dto.response.RideCountRes(
             r.course.id,
@@ -26,14 +26,26 @@ public interface RideRepository extends JpaRepository<Ride, Long>, RideRepositor
         WHERE r.course.id IN :courseIds
         GROUP BY r.course.id
     """)
-    List<RideCountRes> countRidesByCourseId(@Param("courseIds") List<Long> courseIds);
+    List<RideCountRes> countRidesByCourseIds(@Param("courseIds") List<Long> courseIds);
 
     // countRidesByCourseId을 맵으로 변환하는 메서드
     default Map<Long, RideCountRes> countRidesMapByCourseIds(List<Long> courseIds) {
-        return countRidesByCourseId(courseIds).stream()
+        return countRidesByCourseIds(courseIds).stream()
             .collect(Collectors.toMap(
                 RideCountRes::courseId,
                 Function.identity()
             ));
     }
+
+    // courseId와 일치하는 도전자, 완주자 수 조회 메서드 (단건)
+    @Query("""
+        SELECT NEW com.saisai.domain.ride.dto.response.RideCountRes(
+            r.course.id,
+            COUNT(CASE WHEN r.status = 'IN_PROGRESS' THEN 1 END),
+            COUNT(CASE WHEN r.status = 'COMPLETED' THEN 1 END)
+        )
+        FROM Ride r
+        WHERE r.course.id = :courseId
+    """)
+    RideCountRes countRideByCourseId(@Param("courseId") Long courseId);
 }
