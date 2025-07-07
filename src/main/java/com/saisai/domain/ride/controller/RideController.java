52e4242883089d8d3b02b77386b1343e1a374618
duol9 <lhyy0195@gmail.com>
2025-07-08 03:58:46 +0900

    :sparkles: feat: 코스 라이드 시작 API 추가

--- src/main/java/com/saisai/domain/ride/controller/RideController.java
+++ src/main/java/com/saisai/domain/ride/controller/RideController.java
@@ -1,6 +1,16 @@
 package com.saisai.domain.ride.controller;
 
+import static com.saisai.domain.common.response.SuccessCode.RIDE_START_SUCCESS;
+
+import com.saisai.domain.common.response.ApiResponse;
+import com.saisai.domain.ride.dto.response.RideStartRes;
+import com.saisai.domain.ride.service.RideService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,4 +19,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class RideController {
 
+    private final RideService rideService;
+
+    @Operation(summary = "코스 라이딩 시작",
+        description = "총 거리(km), gpx 경로(위도, 경도, 고도, 앞뒤 좌표 간의 거리(m), 현재-시작 좌표 간의 거리(km)")
+    @GetMapping
+    public ResponseEntity<ApiResponse<RideStartRes>> startRide(
+        @PathVariable Long courseId
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success(RIDE_START_SUCCESS, rideService.startRide(courseId)));
+    }
 }
