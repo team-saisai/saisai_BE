fce5f7a25ade43615e609c8819d2a6085874218b
duol9 <lhyy0195@gmail.com>
2025-07-12 16:37:26 +0900

    :sparkles: feat: 코스 저장 API 추가

--- src/main/java/com/saisai/domain/course/controller/CourseSaveController.java
+++ src/main/java/com/saisai/domain/course/controller/CourseSaveController.java
@@ -1,7 +1,21 @@
 package com.saisai.domain.course.controller;
 
+import static com.saisai.domain.common.response.SuccessCode.COURSE_SAVE_SUCCESS;
+
+import com.saisai.config.jwt.AuthUserDetails;
+import com.saisai.domain.common.annotation.Auth;
+import com.saisai.domain.common.response.ApiResponse;
+import com.saisai.domain.course.dto.request.CourseSaveReq;
+import com.saisai.domain.course.dto.response.CourseSaveRes;
+import com.saisai.domain.course.service.CourseSaveService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,5 +24,17 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/saved-courses")
 @RequiredArgsConstructor
 public class CourseSaveController {
+
+    private final CourseSaveService courseSaveService;
+
+    @Operation(summary = "코스 저장")
+    @PostMapping
+    public ResponseEntity<ApiResponse<CourseSaveRes>> saveCourse(
+        @Valid @RequestBody CourseSaveReq courseSaveReq,
+        @Auth AuthUserDetails authUserDetails
+    ) {
+        return ResponseEntity.status(HttpStatus.CREATED)
+            .body(ApiResponse.success(COURSE_SAVE_SUCCESS, courseSaveService.saveCourse(courseSaveReq, authUserDetails)));
+    }
 
 }
