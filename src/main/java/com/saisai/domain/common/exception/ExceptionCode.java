061b5b7ef21ba8d68ae0691494487b9886a95144
duol9 <lhyy0195@gmail.com>
2025-07-12 16:40:41 +0900

    :sparkles: feat: 코스 저장 서비스 로직 추가

--- src/main/java/com/saisai/domain/common/exception/ExceptionCode.java
+++ src/main/java/com/saisai/domain/common/exception/ExceptionCode.java
@@ -31,6 +31,7 @@ public enum ExceptionCode {
     COURSE_NAME_REQUIRED(HttpStatus.BAD_REQUEST, "CR_ER_02", "코스명이 공백으로만 이루어져 있거나 비어있습니다."),
     COURSE_NOT_FOUND(HttpStatus.NOT_FOUND, "CR_ER_03", "코스를 찾을 수 없습니다."),
     COURSE_DISTANCE_INVALID(HttpStatus.BAD_REQUEST, "CR_ER_04", "코스 거리 정보가 유효하지 않습니다."),
+    COURSE_ALREADY_SAVE(HttpStatus.CONFLICT, "CR_ER_05", "이미 저장된 코스 입니다."),
 
     // badge
     BADGE_NAME_DUPLICATE(HttpStatus.BAD_REQUEST, "BG_ER_01", "뱃지명이 중복됩니다."),
--- src/main/java/com/saisai/domain/course/service/CourseSaveService.java
+++ src/main/java/com/saisai/domain/course/service/CourseSaveService.java
@@ -1,10 +1,53 @@
 package com.saisai.domain.course.service;
 
+import static com.saisai.domain.common.exception.ExceptionCode.COURSE_ALREADY_SAVE;
+import static com.saisai.domain.common.exception.ExceptionCode.COURSE_NOT_FOUND;
+import static com.saisai.domain.common.exception.ExceptionCode.USER_NOT_FOUND;
+
+import com.saisai.config.jwt.AuthUserDetails;
+import com.saisai.domain.common.exception.CustomException;
+import com.saisai.domain.course.dto.request.CourseSaveReq;
+import com.saisai.domain.course.dto.response.CourseSaveRes;
+import com.saisai.domain.course.entity.Course;
+import com.saisai.domain.course.entity.CourseSave;
+import com.saisai.domain.course.repository.CourseRepository;
+import com.saisai.domain.course.repository.CourseSaveRepository;
+import com.saisai.domain.user.entity.User;
+import com.saisai.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class CourseSaveService {
 
+    private final CourseRepository courseRepository;
+    private final CourseSaveRepository courseSaveRepository;
+    private final UserRepository userRepository;
+
+    // 코스 저장
+    @Transactional
+    public CourseSaveRes saveCourse(CourseSaveReq courseSaveReq, AuthUserDetails authUserDetails) {
+        Course course = courseRepository.findById(courseSaveReq.courseId())
+            .orElseThrow(() -> new CustomException(COURSE_NOT_FOUND));
+
+        User user = userRepository.findById(authUserDetails.userId())
+            .orElseThrow(() -> new CustomException(USER_NOT_FOUND));
+
+        if (isCourseAlreadySaved(course, user)) {
+            throw new CustomException(COURSE_ALREADY_SAVE);
+        }
+
+        CourseSave courseSave = CourseSave.from(user, course);
+
+        CourseSave saveCourseSave = courseSaveRepository.save(courseSave);
+
+        return CourseSaveRes.from(saveCourseSave);
+    }
+
+    // 코스 저장 이미 존재하는지 확인
+    private boolean isCourseAlreadySaved(Course course, User user) {
+        return courseSaveRepository.existsByCourseIdAndUserId(course.getId(), user.getId());
+    }
 }
