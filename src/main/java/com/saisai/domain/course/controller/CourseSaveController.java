38e97e5844d424f9ba22896fd3b98feb13c9a9a8
duol9 <lhyy0195@gmail.com>
2025-07-12 14:24:01 +0900

    :recycle: refactor: courseLike -> courseSave로 변경

--- src/main/java/com/saisai/domain/course/controller/CourseSaveController.java
+++ src/main/java/com/saisai/domain/course/controller/CourseSaveController.java
@@ -1,12 +1,14 @@
 package com.saisai.domain.course.controller;
 
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "코스 저장 API")
 @RestController
-@RequestMapping("/api/users/{userId}/saved-courses")
+@RequestMapping("/api/saved-courses")
 @RequiredArgsConstructor
-public class CourseLikeController {
+public class CourseSaveController {
 
 }
--- src/main/java/com/saisai/domain/course/service/CourseSaveService.java
+++ src/main/java/com/saisai/domain/course/service/CourseSaveService.java
@@ -5,6 +5,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
-public class CourseLikeService {
+public class CourseSaveService {
 
 }
