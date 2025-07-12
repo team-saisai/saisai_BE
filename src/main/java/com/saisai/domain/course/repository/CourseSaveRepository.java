fc3d56d35cdfa885c61b508f6363542bdfe6a985
duol9 <lhyy0195@gmail.com>
2025-07-12 14:25:09 +0900

    :recycle: refactor: courseLike -> courseSave로 변경

--- src/main/java/com/saisai/domain/course/repository/CourseSaveRepository.java
+++ src/main/java/com/saisai/domain/course/repository/CourseSaveRepository.java
@@ -3,6 +3,6 @@ package com.saisai.domain.course.repository;
 import com.saisai.domain.course.entity.CourseLike;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseLikeRepository extends JpaRepository<CourseLike, Long> {
+public interface CourseSaveRepository extends JpaRepository<CourseLike, Long> {
 
 }
