3c917bcb96aac8d69a396f4848bf4b115f3ff29b
duol9 <lhyy0195@gmail.com>
2025-07-12 17:50:37 +0900

    :sparkles: feat: 코스 북마크 삭제 API 추가

--- src/main/java/com/saisai/domain/common/response/SuccessCode.java
+++ src/main/java/com/saisai/domain/common/response/SuccessCode.java
@@ -17,7 +17,8 @@ public enum SuccessCode {
     COURSE_LIST_GET_SUCCESS(HttpStatus.OK, "COURSE_01", "코스 전체 목록 조회에 성공했습니다."),
     COURSE_INFO_GET_SUCCESS(HttpStatus.OK, "COURSE_02", "코스 단일 상세 조회에 성공했습니다."),
     COURSE_API_SYNC_SUCCESS(HttpStatus.OK, "COURSE_03", "두루누비(코스) API 동기화에 성공했습니다."),
-    COURSE_SAVE_SUCCESS(HttpStatus.CREATED, "COURSE_04", "코스 저장에 성공했습니다."),
+    COURSE_BOOKMARK_ADD_SUCCESS(HttpStatus.CREATED, "COURSE_04", "코스 북마크 추가에 성공했습니다."),
+    COURSE_BOOKMARK_REMOVE_SUCCESS(HttpStatus.OK, "COURSE_05", "코스 북마크 삭제에 성공했습니다."),
 
     // Challenge
     CHALLENGE_POPULAR_LIST_GET_SUCCESS(HttpStatus.OK, "CHALLENGE_04", "인기 챌린지 리스트 조회에 성공했습니다."),
--- src/main/java/com/saisai/domain/course/controller/CourseBookmarkController.java
+++ src/main/java/com/saisai/domain/course/controller/CourseBookmarkController.java
@@ -1,6 +1,7 @@
 package com.saisai.domain.course.controller;
 
-import static com.saisai.domain.common.response.SuccessCode.COURSE_SAVE_SUCCESS;
+import static com.saisai.domain.common.response.SuccessCode.COURSE_BOOKMARK_ADD_SUCCESS;
+import static com.saisai.domain.common.response.SuccessCode.COURSE_BOOKMARK_REMOVE_SUCCESS;
 
 import com.saisai.config.jwt.AuthUserDetails;
 import com.saisai.domain.common.annotation.Auth;
@@ -18,7 +19,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Tag(name = "코스 저장 API")
+@Tag(name = "코스 북마크 API")
 @RestController
 @RequestMapping("/api/courses/{courseId}/bookmarks")
 @RequiredArgsConstructor
@@ -26,14 +27,24 @@ public class CourseBookmarkController {
 
     private final CourseBookmarkService courseBookMarkService;
 
-    @Operation(summary = "코스 저장")
+    @Operation(summary = "코스 북마크 추가")
     @PostMapping
     public ResponseEntity<ApiResponse<CourseBookmarkRes>> bookmarkCourse(
         @PathVariable Long courseId,
         @Auth AuthUserDetails authUserDetails
     ) {
         return ResponseEntity.status(HttpStatus.CREATED)
-            .body(ApiResponse.success(COURSE_SAVE_SUCCESS, courseBookMarkService.bookmarkCourse(courseId, authUserDetails)));
+            .body(ApiResponse.success(COURSE_BOOKMARK_ADD_SUCCESS, courseBookMarkService.bookmarkCourse(courseId, authUserDetails)));
+    }
+
+    @Operation(summary = "코스 북마크 취소")
+    @DeleteMapping()
+    public ResponseEntity<ApiResponse<CourseBookmarkRes>> removeBookmark(
+        @PathVariable Long courseId,
+        @Auth AuthUserDetails authUserDetails
+    ) {
+        return ResponseEntity.status(HttpStatus.OK)
+            .body(ApiResponse.success(COURSE_BOOKMARK_REMOVE_SUCCESS,courseBookMarkService.removeBookmark(courseId, authUserDetails)));
     }
 
 }
