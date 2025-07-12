b5a89f0c79803a5a14a92487a1813c1bddc1e155
duol9 <lhyy0195@gmail.com>
2025-07-12 16:40:23 +0900

    :sparkles: feat: 코스 중복 저장 방지를 위한 존재 여부 확인 메서드 추가

--- src/main/java/com/saisai/domain/course/repository/CourseSaveRepository.java
+++ src/main/java/com/saisai/domain/course/repository/CourseSaveRepository.java
@@ -5,4 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseSaveRepository extends JpaRepository<CourseSave, Long> {
 
+    boolean existsByCourseIdAndUserId(Long courseId, Long userId);
 }
