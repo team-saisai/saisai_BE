c23ea6f65012da07354c554a9a77d0954e2f9664
duol9 <lhyy0195@gmail.com>
2025-07-13 15:38:00 +0900

    :sparkles: feat: 리워드이벤트 등록 API 추가

--- src/main/java/com/saisai/domain/common/response/SuccessCode.java
+++ src/main/java/com/saisai/domain/common/response/SuccessCode.java
@@ -35,6 +35,9 @@ public enum SuccessCode {
     MY_BADGE_LIST_GET_SUCCESS(HttpStatus.OK, "BADGE_02", "로그인 한 사용자의 뱃지 리스트 조회에 성공했습니다."),
     MY_BADGE_DETAIL_GET_SUCCESS(HttpStatus.OK, "BADGE_03", "뱃지 상세 조회에 성공했습니다."),
 
+    // reward_event
+    REWARD_EVENT_CREATE_SUCCESS(HttpStatus.CREATED, "EVENT_01", "리워드 이벤트 생성에 성공했습니다."),
+
     ;
 
     private final HttpStatus httpStatus;
