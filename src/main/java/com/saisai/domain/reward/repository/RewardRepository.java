f8a8d015efaeeb26b7a0d634b879198890e9f41a
duol9 <lhyy0195@gmail.com>
2025-07-13 15:32:42 +0900

    :wastebasket: remove: Reward 엔티티 삭제

--- src/main/java/com/saisai/domain/reward/repository/RewardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.saisai.domain.reward.repository;
-
-import com.saisai.domain.reward.entity.Reward;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RewardRepository extends JpaRepository<Reward, Long> {
-
-}
