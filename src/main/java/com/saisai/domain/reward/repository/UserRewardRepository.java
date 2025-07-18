a3815ebeea85e3bfde8b62abb40c057337c810a2
duol9 <lhyy0195@gmail.com>
2025-07-19 03:28:43 +0900

    :sparkles: feat: UserReward List 조회 메서드 추가

--- src/main/java/com/saisai/domain/reward/repository/UserRewardRepository.java
+++ src/main/java/com/saisai/domain/reward/repository/UserRewardRepository.java
@@ -1,8 +1,18 @@
 package com.saisai.domain.reward.repository;
 
 import com.saisai.domain.reward.entity.UserReward;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface UserRewardRepository extends JpaRepository<UserReward, Long> {
+
+    @Query("SELECT ur "
+        + "FROM UserReward ur "
+        + "JOIN FETCH ur.course "
+        + "WHERE ur.user.id = :userId "
+        + "ORDER BY ur.createdAt DESC")
+    List<UserReward> findAllByUserId(@Param("userId") Long userId);
 
 }
