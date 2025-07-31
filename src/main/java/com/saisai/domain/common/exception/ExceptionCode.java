e52009b2b10dc4c1ea8c66d9c4045afe23d9347e
duol9 <lhyy0195@gmail.com>
2025-07-31 16:24:11 +0900

    :sparkles: feat: CourseType enum 추가

--- src/main/java/com/saisai/domain/common/exception/ExceptionCode.java
+++ src/main/java/com/saisai/domain/common/exception/ExceptionCode.java
@@ -33,6 +33,7 @@ public enum ExceptionCode {
     COURSE_DISTANCE_INVALID(HttpStatus.BAD_REQUEST, "CR_ER_04", "코스 거리 정보가 유효하지 않습니다."),
     COURSE_ALREADY_BOOKMARK(HttpStatus.CONFLICT, "CR_ER_05", "이미 북마크된 코스입니다."),
     COURSE_BOOKMARK_NOT_FOUND(HttpStatus.NOT_FOUND, "CR_ER_06", "코스 북마크를 찾을 수 없습니다."),
+    COURSE_TYPE_NOT_FOUND(HttpStatus.NOT_FOUND, "CR_ER_08", "해당 코스 타입을 찾을 수 없습니다."),
 
     // badge
     BADGE_NAME_DUPLICATE(HttpStatus.BAD_REQUEST, "BG_ER_01", "뱃지명이 중복됩니다."),
