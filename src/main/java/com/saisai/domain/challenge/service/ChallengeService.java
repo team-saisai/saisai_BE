56a5f9059eaece4f50f828d8a0385fce0eaba934
duol9 <lhyy0195@gmail.com>
2025-08-02 04:27:31 +0900

    :sparkles: feat: Dto 통합으로 인한 로직 추가

--- src/main/java/com/saisai/domain/challenge/service/ChallengeService.java
+++ src/main/java/com/saisai/domain/challenge/service/ChallengeService.java
@@ -1,16 +1,15 @@
 package com.saisai.domain.challenge.service;
 
-import com.saisai.domain.challenge.dto.projection.ChallengeCardProjection;
+import com.saisai.config.jwt.AuthUserDetails;
+import com.saisai.domain.challenge.dto.response.PopularChallengeCourseRes;
 import com.saisai.domain.challenge.repository.ChallengeRepository;
 import com.saisai.domain.common.aws.s3.ImageUtil;
-import com.saisai.domain.course.dto.projection.CourseCardProjection;
-import com.saisai.domain.course.dto.response.CourseCardRes;
-import com.saisai.domain.course.repository.CourseRepository;
+import com.saisai.domain.course.dto.projection.ChallengeCourseProjection;
+import com.saisai.domain.reward.dto.projection.RewardEventProjection;
+import com.saisai.domain.reward.util.RewardUtils;
 import java.util.Collections;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -33,12 +32,32 @@ public class ChallengeService {
         }
 
         return popularChallengeInfos.stream()
-            .map(popularChallengesInfo -> {
-                CourseCardProjection courseInfo = courseCardMap.get(popularChallengesInfo.courseId());
-                String courseImageUrl = imageUtil.getImageUrl(courseInfo.image());
+            .map(popularChallengeInfo -> {
+                String imageUrl = imageUtil.getImageUrl(popularChallengeInfo.imageUrl());
+                boolean isEventActive = isRewardEventActive(popularChallengeInfo.rewardEventProjection());
+                int reward = calculateReward(popularChallengeInfo, isEventActive);
 
-                return CourseCardRes.from(popularChallengesInfo, courseInfo, courseImageUrl);
+                return PopularChallengeCourseRes.from(popularChallengeInfo, imageUrl, isEventActive, reward);
             })
             .toList();
     }
+
+    // 이벤트 활성화 확인
+    private boolean isRewardEventActive(RewardEventProjection rewardEventProjection) {
+        return Optional.ofNullable(rewardEventProjection)
+            .map(RewardEventProjection::rewardEventId)
+            .isPresent();
+    }
+
+    // 리워드 계산
+    private int calculateReward(ChallengeCourseProjection challengeCourseProjection, boolean isEventActive) {
+        return isEventActive ?
+            RewardUtils.calculateEventReward(
+                challengeCourseProjection.level(),
+                challengeCourseProjection.rewardEventProjection().rewardEventType(),
+                challengeCourseProjection.rewardEventProjection().value()) :
+            RewardUtils.calculateEventReward(challengeCourseProjection.level());
+    }
+
+
 }
--- src/main/java/com/saisai/domain/course/repository/CourseRepositoryCustom.java
+++ src/main/java/com/saisai/domain/course/repository/CourseRepositoryCustom.java
@@ -2,8 +2,8 @@ package com.saisai.domain.course.repository;
 
 import com.saisai.domain.course.constant.CourseSortOption;
 import com.saisai.domain.course.dto.projection.ChallengeCourseProjection;
+import com.saisai.domain.course.dto.projection.CourseDetailsProjection;
 import com.saisai.domain.course.dto.projection.GeneralCourseProjection;
-import java.util.List;
 import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
