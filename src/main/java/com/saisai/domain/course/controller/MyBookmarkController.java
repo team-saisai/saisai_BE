22503e0eaff805b830460567d3391b0b285db5fc
duol9 <lhyy0195@gmail.com>
2025-08-04 06:04:22 +0900

    :recycle: refactor: 저장한 코스 조회 API에서 정렬 및 타입 파라미터 제거

--- src/main/java/com/saisai/domain/course/controller/MyBookmarkController.java
+++ src/main/java/com/saisai/domain/course/controller/MyBookmarkController.java
@@ -6,8 +6,6 @@ import static com.saisai.domain.common.response.SuccessCode.COURSE_BOOKMARK_GET_
 import com.saisai.config.jwt.AuthUserDetails;
 import com.saisai.domain.common.annotation.Auth;
 import com.saisai.domain.common.response.ApiResponse;
-import com.saisai.domain.course.constant.CourseSortOption;
-import com.saisai.domain.course.constant.CourseType;
 import com.saisai.domain.course.dto.request.BookmarksRemoveReq;
 import com.saisai.domain.course.dto.response.BookmarksRemoveRes;
 import com.saisai.domain.course.dto.response.CoursePageRes;
@@ -19,7 +17,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -47,25 +44,20 @@ public class MyBookmarkController {
             .body(ApiResponse.success(COURSE_BOOKMARK_EDIT_SUCCESS, courseBookmarkService.removeBookmarks(bookmarksRemoveReq, authUserDetails)));
     }
 
-    @Operation(summary = "저장한 코스 조회")
+    @Operation(summary = "저장한 코스 조회",
+    description = "최근 저장한 순으로 정렬\n\n코스 조회 response 구조와 동일\n\n챌린지가 아닌 코스는 (challengeStatus, challengeEndedAt, isEventActive, reward) = null")
     @GetMapping
     public ResponseEntity<ApiResponse<Page<CoursePageRes>>> removeBookmarks(
         @Parameter(description = "페이지 번호") @RequestParam(defaultValue = "1") int page,
-        @Parameter(description = "challenge(챌린지 코스), general(일반 코스)") @RequestParam(defaultValue = "challenge") String type,
-        @Parameter(description = "levelAsc(난이도 낮은 순), levelDesc(난이도 높은 순), participantsDesc(참가자 순), endSoon(종료일 순)") @RequestParam(defaultValue = "levelAsc") String sort,
         @Auth AuthUserDetails authUserDetails
     ) {
-        CourseType courseType = CourseType.from(type);
-        CourseSortOption sortOption = CourseSortOption.from(sort);
-
         Pageable pageable = PageRequest.of(
             page - 1,
-            10,
-            Sort.by(sortOption.getDirection(), sortOption.getSortColumn())
+            10
         );
 
         return ResponseEntity.status(HttpStatus.OK)
-            .body(ApiResponse.success(COURSE_BOOKMARK_GET_SUCCESS, courseBookmarkService.getBookmarkCourses(pageable, courseType, sortOption, authUserDetails)));
+            .body(ApiResponse.success(COURSE_BOOKMARK_GET_SUCCESS, courseBookmarkService.getBookmarkCourses(pageable, authUserDetails)));
     }
 
 }
