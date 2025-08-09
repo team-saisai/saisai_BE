b8ca3e16f95ed95a65ac3b212a52c88a4b4cfbd2
duol9 <lhyy0195@gmail.com>
2025-08-10 03:32:57 +0900

    :sparkles: feat: CheckpointRes 추가

---
+++ src/main/java/com/saisai/domain/course/dto/response/CheckpointRes.java
@@ -0,0 +1,12 @@
+package com.saisai.domain.course.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true) // 파싱 안해도 될 필드가 있음
+public record CheckpointRes(
+    @JsonProperty("latitude") Double lat,
+    @JsonProperty("longitude") Double lon
+) {
+
+}
