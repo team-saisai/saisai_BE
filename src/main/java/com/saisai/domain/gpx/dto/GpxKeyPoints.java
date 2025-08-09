32a183bd347b9d59f7d74bd322f619ba51f57f21
duol9 <lhyy0195@gmail.com>
2025-08-09 20:46:20 +0900

    :sparkles: feat:  FirstGpxPoint에 min/max 위도·경도 필드 추가

---
+++ src/main/java/com/saisai/domain/gpx/dto/GpxKeyPoints.java
@@ -0,0 +1,14 @@
+package com.saisai.domain.gpx.dto;
+
+public record FirstGpxPoint(
+    Double lat,
+    Double lon
+    Double startLat,
+    Double startLon,
+    Double minLat,
+    Double minLon,
+    Double maxLat,
+    Double maxLon
+) {
+
+}
