b55f9ffc7f9ec5d24e477f4b245d9862ba64903a
duol9 <lhyy0195@gmail.com>
2025-08-16 02:19:47 +0900

    :recycle: refactor: 카카오 로그인 디버깅 메서드 삭제

--- src/main/java/com/saisai/domain/auth/oauth/kakao/client/KakaoClient.java
+++ src/main/java/com/saisai/domain/auth/oauth/kakao/client/KakaoClient.java
@@ -25,28 +25,4 @@ public class KakaoClient {
 
         return UserInfo.from(kakaoUser);
     }
-
-    // 💡 디버깅을 위한 임시 테스트 메서드
-    public UserInfo testWithHardcodedToken(String testToken) {
-        System.out.println("디버깅 테스트 시작");
-        try {
-            KakaoUser kakaoUser = RestClient.create()
-                .get()
-                .uri("https://kapi.kakao.com/v2/user/me")
-                .header(HttpHeaders.AUTHORIZATION, "Bearer " + testToken)
-                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_FORM_URLENCODED_VALUE)
-                .retrieve()
-                .onStatus(HttpStatusCode::is4xxClientError, (req, res) -> {
-                    System.err.println("디버깅 실패: " + res.getStatusCode());
-                    throw new RuntimeException("카카오 사용자 정보 요청에 실패하였습니다.");
-                })
-                .body(KakaoUser.class);
-
-            System.out.println("디버깅 성공! 사용자 이름: " + kakaoUser.kakaoAccount().name());
-            return UserInfo.from(kakaoUser);
-        } catch (Exception e) {
-            System.err.println("디버깅 중 예외 발생: " + e.getMessage());
-        }
-        return null;
-    }
 }
