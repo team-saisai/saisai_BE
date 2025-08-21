0532e23658b38dd8b8eae45d6b2d757245f76e49
duol9 <lhyy0195@gmail.com>
2025-08-21 17:11:39 +0900

    :recycle: refactor: 닉네임 변경 조건 변경

--- src/main/java/com/saisai/domain/user/annotation/ValidNickname.java
+++ src/main/java/com/saisai/domain/user/annotation/ValidNickname.java
@@ -20,7 +20,7 @@ import java.lang.annotation.Target;
 
 @Pattern(regexp = "^[가-힣a-zA-Z0-9]{1,7}$", message = "이름은 1자 이상 7자 이하의 한글, 영어, 숫자만 사용할 수 있습니다.")
 @NotBlank(message = "닉네임은 공백일 수 없습니다.")
-@Size(min = 1, max = 10, message = "닉네임은 1자 이상 10자 이하로 입력해주세요.")
+@Size(min = 1, max = 7, message = "닉네임은 1자 이상 7자 이하로 입력해주세요.")
 public @interface ValidNickname {
 
     String message() default "닉네임이 유효하지 않습니다.";
